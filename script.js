79da73be4bac284adaf759101f54492582607b0f
devraj sawant <devcodes611@gmail.com>
2024-01-21 07:36:31 +0530

    third commit done

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ document.querySelector('#searchbtn').onclick = () =>{
     searchform.classList.toggle('active');
 }
 
+let loginform = document.querySelector('.loginformcontainer');
+
+document.querySelector('#userbtn').onclick = () =>{
+  loginform.classList.toggle('act');
+}
+
+
+document.querySelector('#closeloginbutton').onclick = () =>{
+  loginform.classList.remove('act');
+}
+
 window.onscroll = () => {
 
     searchform.classList.toggle('active');
@@ -27,6 +38,8 @@ window.onload = () => {
 
 
 
+
+
   var swiper = new Swiper(".bookslider", {
    loop:true,
    centeredslides :true,
@@ -47,6 +60,32 @@ window.onload = () => {
     },
   });
 
-
+  var swiper = new Swiper(".featuredslider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredslides :true,
+    autoplay : {
+     display:9500,
+     disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     breakpoints: {
+       0: {
+         slidesPerView: 1, 
+        450: {
+          slidesPerView: 2,
+        },
+       },
+       768: {
+         slidesPerView: 3,
+       },
+       1024: {
+         slidesPerView: 4,
+       },
+     },
+   });
 
 
